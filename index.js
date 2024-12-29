6318d639e8b97fddd804de65eae2f6b67041269e
Cmaster <146499846+vaibhavjathar@users.noreply.github.com>
2024-12-29 15:32:52 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+import { createStore, applyMiddleware } from "redux";
+import { thunk as thunkMiddleware } from 'redux-thunk';
+
+import rootReducer from "../reducers";
+
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+
+export default store;
