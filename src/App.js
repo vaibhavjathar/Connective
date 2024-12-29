df3972bc1e91bce3ba6b7547ef11cf99ff13ef71
Cmaster <146499846+vaibhavjathar@users.noreply.github.com>
2024-12-29 15:25:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// import './App.css';
+// import Header from './components/Header';
+// import Home from './components/Home';
+// import Login from "./components/login";
+// function App() {
+//   return (
+//     <div className="App">
+//       <Router>
+//         <Routes>
+//           <Route exact path="/" element={<Login />}/>
+//           <Route path= "/home" element={<Home />}/>
+//           <Route path= "/home" element={<Header />}/>
+//         </Routes>
+//       </Router>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useEffect } from "react";
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
 import Login from "./components/login";
-function App() {
+import { getUserAuth } from './actions';
+import { connect } from "react-redux";
+function App(props) {
+  useEffect(() => {
+    props.getUserAuth();
+  }, []);
   return (
     <div className="App">
       <Router>
-        <Switch>
-          <Route exact path="/">
-           <Login />
-          </Route>
-          <Route path= "/home">
-           <Header /> 
-           <Home />
-          </Route>
-        </Switch>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
       </Router>
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {};
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  getUserAuth: () => dispatch(getUserAuth()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
